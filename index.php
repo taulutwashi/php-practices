79827ed1b98027fa92ba12aad92aea52521c487f
Taulut Hossain Washi <taulut.hossain@amiprobashi.com>
2024-04-28 22:18:02 +0600

    DEV: initiate PDO database connection

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 require 'functions.php';
 
-require 'router.php';
+//require 'router.php';
+
+$dsn = "mysql:host=localhost;port=3306;dbname=php_practices;charset=UTF8";
+
+$pdo = new PDO($dsn, 'root','washi');
+$query = $pdo->prepare('SELECT * FROM posts');
+$query->execute();
+
+$posts = $query->fetchAll(PDO::FETCH_ASSOC);
+
+dd($posts);
 
